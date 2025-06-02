5934f53b0447a130d03d7db4b3e69f820e0a4098
LuigiMDev <luigicumaru@gmail.com>
2025-06-02 12:41:15 -0300

    feat: ajuste na responsividade da hero

--- src/app/components/BackgroundVideo.tsx
+++ src/app/components/BackgroundVideo.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BackgroundVideo = () => {
   return (
-    <div className='absolute inset-0 bg-black overflow-hidden w-screen h-screen -z-1'>
+    <div className='absolute inset-x-0 top-0 bg-black overflow-hidden w-screen h-[100dvh] -z-1'>
         <video src="/particle_bg_loop.mp4" className='object-cover w-full h-full opacity-40' autoPlay loop muted></video>
     </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
-import Image from "next/image";
-import ThemeSwitch from "./components/ThemeSwitch";
 import BackgroundVideo from "./components/BackgroundVideo";
+import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="relative w-screen h-screen flex flex-col justify-center items-center">
+    <div className="relative w-screen flex flex-col justify-center items-center">
       <BackgroundVideo />
-      <ThemeSwitch />
-      <HeroSection />
+      <Header />
+      <main className="w-full">
+        <HeroSection />
+        <section className="relative text-white overflow-hidden">
+          <div className="absolute inset-0 bg-gradient-to-b from-[#1a1a2e]/0 via-[#0f0f1f]  to-[#0f0f1f]"></div>
+
+          <div className="absolute inset-0 bg-[url('/noise.png')] opacity-5 mix-blend-overlay"></div>
+
+          <div className="relative z-10 px-6 py-20 max-w-5xl mx-auto">
+            <h2 className="text-3xl font-bold mb-4">Sobre mim</h2>
+            <p className="text-lg text-gray-300">
+              Aqui começa a seção com mais conteúdo...
+            </p>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
